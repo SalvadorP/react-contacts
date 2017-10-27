2d24280957b30ea4138ccca6e24726409c0931db
Salvador P <salvadorperezd@gmail.com>
2017-10-27 16:51:10 +0200

    Lesson 4 finished
    Added the ContactsAPI
    Now the app can remove contacts and fetch contacts from the backend
    server.

--- src/App.js
+++ src/App.js
@@ -2,33 +2,22 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import ListContacts from './ListContacts';
 import ListContactsF from './ListContacts';
-
+import * as ContactsAPI from './utils/ContactsAPI';
 
 // The props in the components are like the parameters on the functions.
 class App extends Component {
   // NEVER initialize the state using props!!
   state = {
-    contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]    
+    contacts: []    
   }
+
+  componentDidMount() {
+    // ContactsAPI getAll returns a JS promise, if all is ok it will return contacts array so we can change the state with it.
+    ContactsAPI.getAll().then((contacts) => {
+      this.setState({contacts})
+    })
+  }
+
   removeContact = (contact) => {
     // The setState can be invoked in two ways
     // The reason to use the setState with the function is to make more operations. 
@@ -37,6 +26,9 @@ class App extends Component {
     this.setState((state) => ({
       contacts: state.contacts.filter((c) => c.id !== contact.id)
     }))
+
+    // Now we are removing the contact at the backend server.
+    ContactsAPI.remove(contact);
 
     // This way you only replace the state with a literal or something.
     // this.setState({ })    
